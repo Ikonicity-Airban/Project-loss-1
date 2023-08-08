c1f70f8112e2b7e6d01ac05d26ce3620a5634fd4
Ikonicity Airban <ikonicityairban@gmail.com>
2023-08-08 07:38:52 +0100

    okay

--- frontend/src/pages/student/assignment.tsx
+++ frontend/src/pages/student/assignment.tsx
@@ -1,9 +1,35 @@
+import { IAssignment } from "../../api/@types";
+import { ListGroup } from "flowbite-react";
+import Section from "../../components/Section";
+import useAxiosPrivate from "../../api/hooks/useAxiosPrivate";
 import { useQuery } from "react-query";
-import { getAssignment } from "../../api/resource/assignment";
 
+// const gridStyle = { minHeight: 550, minWidth: 860 };
 function AssignmentPage() {
-  const { data } = useQuery("assignment", getAssignment);
-  return <div>{JSON.stringify(data)}</div>;
+  const http = useAxiosPrivate();
+  const { data: assignment } = useQuery(
+    "assignment",
+    async () => await http.get("/assignment")
+  );
+  return (
+    <main className="my-10">
+      <ListGroup>
+        <ListGroup.Item>
+          <Section title="Registered Courses">
+            <div className="overflow-auto z-10">
+              {/* add new course */}
+              <div className="grid mobile:grid-cols-2 tablet:grid-cols-3">
+                {assignment?.data.map((ass: IAssignment) => (
+                  <>{ass.title}</>
+                ))}
+              </div>
+            </div>
+            {/* all courses semester by semester list add and delete */}
+          </Section>
+        </ListGroup.Item>
+      </ListGroup>
+    </main>
+  );
 }
 
 export default AssignmentPage;
